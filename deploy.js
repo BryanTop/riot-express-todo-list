0a21dd9f9665378f9431b9f8ff26a8fd236bd140
RwbyPanda <whitefox13@outlook.com>
2019-09-02 14:51:49 -0700

    fixing travis

--- deploy.js
+++ deploy.js
@@ -22,7 +22,7 @@ function installPM2() {
 // transfers local project to the remote server
 function transferProjectToRemote(failed, successful) {
   return ssh.putDirectory(
-    '../riot-starter',
+    'C:/Users/white/Desktop/riot-starter',
     '/home/ubuntu/riot-starter-temp',
     {
       recursive: true,
